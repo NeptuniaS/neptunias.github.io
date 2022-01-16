db07033ce000cd9ccd864988574ca3bf05c7577a
QIYE SUI <nepsky>
2022-01-16 16:33:20 +0900

    first post

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -8,30 +8,30 @@ module.exports = (options, context, api) => {
       directories: [
         {
           id: "en",
-          dirname: "_en",
+          dirname: "_blog",
           title: "Post",
-          path: "/en/",
-          itemPermalink: "/en/:year/:month/:day/:slug"
+          path: "/blog/",
+          itemPermalink: "/blog/:year/:month/:day/:slug"
         }
       ],
       sitemap: {
-        hostname: "https://billyyyyy3320.com/"
+        hostname: "https://neptunias.com/"
       },
       // comment: {
       //   service: "vssue",
       //   autoCreateIssue: true,
-      //   prefix: "[Post]",
-      //   owner: "newsbielt703",
-      //   repo: "billy",
-      //   clientId: "4119e8c1b0093fc5d034",
-      //   clientSecret: "1ac1176791689b1ca31037c39489fc7b0667015d"
+      //   prefix: "",
+      //   owner: "",
+      //   repo: "",
+      //   clientId: "",
+      //   clientSecret: ""
       // },
       // newsletter: {
       //   endpoint:
-      //     "https://gmail.us5.list-manage.com/subscribe/post?u=942c0d587f8ea28269e80d6cd&amp;id=d77d789d53"
+      //     ""
       // },
       // feed: {
-      //   canonical_base: "https://billyyyyy3320.com/",
+      //   canonical_base: "",
       //   posts_directories: ["/_en/"]
       // },
       nav: [
